80efc3e8c0147b78e405b1191a168a5e8fbe3647
AyushMishra504 <ayushmishra.5a@gmail.com>
2024-10-05 01:46:29 +0530

    Update app.js

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,38 +1,58 @@
-/* eslint-disable no-unused-vars */
-const {request,response} = require('express')
-const express = require('express')//imports the EXPRESS module installed via NPM
-const app= express() 
-const {Todo} = require("./models")
-const bodyParser = require("body-parser")
-app.use(bodyParser.json())
+const express = require("express");
+const app = express();
+const { Todo } = require("./models");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
 
-app.get("/todos",(request,response) => {
-    console.log("Todo List")
-})
+app.get("/", function (request, response) {
+  response.send("Hello World");
+});
 
-app.post("/todos", async (request,response) => {
-    console.log("creating a Todo", request.body)
-    try {
-        const todo= await Todo.addTodo({title:request.body.title, dueDate: request.body.dueDate, completed: false })
-        return response.json(todo)
-    }catch (error) {
-            console.log(error)
-            return response.status(422).json(error)
-    }})
+app.get("/todos", async function (_request, response) {
+  console.log("Processing list of all Todos ...");
+  const todos = await Todo.findAll();
+  response.json(todos);
+});
 
-app.put("/todos/:id/markAsCompleted",async (request,response) => {
-    console.log("We have to update a Todo with ID", request.params.id)
-    const todo= await Todo.findByPk(request.params.id)
-    try{
-        const updated_todo=await todo.markAsCompleted()
-        return response.json(updated_todo)
-}catch(error){
-    console.log(error)
-    return response.status(422).json(error)
-}})
+app.get("/todos/:id", async function (request, response) {
+  try {
+    const todo = await Todo.findByPk(request.params.id);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
 
-app.delete("/todos/:id",(request,response) => {
-    console.log("Delete a Todo with ID", request.params.id)
-    
-})
-module.exports = app
+app.post("/todos", async function (request, response) {
+  try {
+    const todo = await Todo.addTodo(request.body);
+    return response.json(todo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.put("/todos/:id/markAsCompleted", async function (request, response) {
+  const todo = await Todo.findByPk(request.params.id);
+  try {
+    const updatedTodo = await todo.markAsCompleted();
+    return response.json(updatedTodo);
+  } catch (error) {
+    console.log(error);
+    return response.status(422).json(error);
+  }
+});
+
+app.delete("/todos/:id", async function (request, response) {
+  console.log("We have to delete a Todo with ID: ", request.params.id);
+  const result = await Todo.destroy({ where: { id: request.params.id } });
+  if (result > 0) {
+    return response.json(true);
+  } else {
+    return response.json(false);
+  }
+});
+
+module.exports = app;
